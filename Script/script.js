5c8bb2983759f83dcb592d7ce2b81bbd79392187
Delacoudre Willelm <delacoudrew@gmail.com>
2025-09-19 21:15:39 +0200

    changing color palet and arraging the ui

---
+++ Script/script.js
@@ -0,0 +1,15 @@
+let elt = document.getElementById('aboutMyself')
+let elt2 = document.getElementById('language')
+let elt3 = document.getElementById('myProjects')
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('animation_entrance_2');
+        } 
+    });
+});
+
+observer.observe(elt);
+observer.observe(elt2);
+observer.observe(elt3);
