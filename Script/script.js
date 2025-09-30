1a0ea46f3f676f23f8795a85d1cbea3e6691337e
Delacoudre Willelm <delacoudrew@gmail.com>
2025-09-30 09:15:11 +0200

    (UPDATE) trying css animation

--- Script/script.js
+++ Script/script.js
@@ -1,15 +1,27 @@
-let elt = document.getElementById('aboutMyselfTitle')
-let elt2 = document.getElementById('language')
-let elt3 = document.getElementById('myProjects')
+let elt = document.getElementById("aboutMyselfTitle");
+let elt2 = document.getElementById("language");
+let elt3 = document.getElementById("myProjects");
+let elt4 = document.getElementById("animaDiv");
 
 const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("animation_entrance_2");
+    }
+  });
+});
+
+const observer2 = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
-            entry.target.classList.add('animation_entrance_2');
-        } 
-    });
-});
+            console.log("dedans")
+            entry.target.classList.add("-translate-y-1000")
+            entry.target.classList.add("duration-3000")
+        }
+    })
+})
 
 observer.observe(elt);
 observer.observe(elt2);
 observer.observe(elt3);
+observer2.observe(elt4);
