e7ed326064320462b13a2de5c03495474cd1d766
Delacoudre Willelm <delacoudrew@gmail.com>
2025-10-01 21:43:48 +0200

    (UPDATE) update main page
    update main page with css and content

--- Script/script.js
+++ Script/script.js
@@ -2,6 +2,7 @@ let elt = document.getElementById("aboutMyselfTitle");
 let elt2 = document.getElementById("language");
 let elt3 = document.getElementById("myProjects");
 let elt4 = document.getElementById("animaDiv");
+let elt5 = document.getElementById("welcomeDiv");
 
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
@@ -12,16 +13,28 @@ const observer = new IntersectionObserver((entries) => {
 });
 
 const observer2 = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            console.log("dedans")
-            entry.target.classList.add("-translate-y-1000")
-            entry.target.classList.add("duration-3000")
-        }
-    })
-})
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      console.log("dedans");
+      entry.target.classList.add("-translate-y-1000");
+      entry.target.classList.add("duration-3000");
+    }
+  });
+});
 
-observer.observe(elt);
-observer.observe(elt2);
-observer.observe(elt3);
-observer2.observe(elt4);
+function delayAnimation(value, classe) {
+  setTimeout(() => {
+    value.classList.add(classe);
+    value.classList.remove("hidden");
+  }, 700);
+}
+
+function onStart() {
+  delayAnimation(elt5, "animation_entrance");
+  observer.observe(elt);
+  observer.observe(elt2);
+  observer.observe(elt3);
+  observer2.observe(elt4);
+}
+
+onStart();
